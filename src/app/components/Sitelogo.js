0ea0b46d3e3cbe4795db36712ba73b62e278c10b
Muhammad Mustafa <mustafa25008c@gmail.com>
2026-07-13 12:21:18 +0500

    fix: change logo

--- src/app/components/Sitelogo.js
+++ src/app/components/Sitelogo.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Sitelogo(){
     return (
         <Image
-        src="/logo.png" // Path relative to public folder (or external URL)
+        src="/logo1.png" // Path relative to public folder (or external URL)
         alt="Logo"
         fill
         priority // Loads the image faster if it is above the fold
